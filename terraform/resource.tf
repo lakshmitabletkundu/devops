7d594824b443793a85a0c83281b1afd9e9c72e90
lakshmitabletkundu <lakshmi.tablet.kundu@gmail.com>
2025-03-25 00:51:19 +0530

    add acr

--- terraform/resource.tf
+++ terraform/resource.tf
@@ -2,3 +2,16 @@ resource "azurerm_resource_group" "rg"{
    name= "${var.prefix}-res-001"
    location= var.location
 }
+
+resource "azurerm_container_registry" "acrlakshmimy" {
+  name                = "containerRegistry1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Premium"
+  admin_enabled       = false
+  georeplications {
+    location                = "East US"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
+}
